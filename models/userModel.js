163cf04b824b01cadc2d94adb86e856606061b11
Alexandru Florin Pop <popflorin1705@yahoo.com>
2017-05-25 12:32:14 +0300

    pug + show user data after github auth

--- models/userModel.js
+++ models/userModel.js
@@ -2,19 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    "githubId": {
+        type: String,
+        required: true,
+        unique: true
+    },
     "username": {
         type: String,
         required: true,
         unique: true
     },
+    "avatar_url": {
+        type: String
+    },
     "email": {
         type: String,
         required: true,
         unique: true
-    },
-    "password": {
-        type: String,
-        required: true
     },
     "create_at": {
         type: Date,
--- server.js
+++ server.js
@@ -5,27 +5,63 @@ const passport = require('passport');
 const GitHubStrategy = require('passport-github').Strategy;
 const mongoose = require('mongoose');
 const app = express();
+const pug = require('pug');
+
+mongoose.connect('mongodb://localhost/github');
 
 // constants
 const PORT = process.env.PORT || 8080;
 const config = require('./config');
 const User = require('./models/userModel');
 
+// passport
+passport.use(new GitHubStrategy( config.githubAuth, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ githubId: profile.id }, (err, user) => {
+        if (err) return done(err, user);
+
+        if(!user) {
+            let user = {
+                githubId: profile.id,
+                username: profile.username,
+                avatar_url: profile._json.avatar_url,
+                email: profile._json.email
+            }
+            User.create(user, (err, user) => {
+                return done(err, user);
+            });
+        } else {
+            return done(err, user);
+        }
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
 // middlewares
+app.set('views', './public');
+app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// passport
-passport.use(new GitHubStrategy( config.githubAuth,
-    function(accessToken, refreshToken, profile, cb) {
-        User.findOrCreate({ githubId: profile.id }, function (err, user) {
-            return cb(err, user);
-        });
-    }
-));
+app.get('/auth/github',
+    passport.authenticate('github'));
 
-// routes
+app.get('/profile',
+    passport.authenticate('github', { failureRedirect: '/login' }),
+        function(req, res) {
+            res.render('profile', req.user);
+            // res.redirect('/profile?user=' + JSON.stringify(req.user), { "mama": "yeah" });
+    });
+
 app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/public/login.html');
 });
